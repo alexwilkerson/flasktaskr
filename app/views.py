e29e594681c5d64d6ce28ddd7305d2ff09752d27
Alex Wilkerson <alex@uncorrected.com>
2016-01-17 04:20:14 -0600

    finished part 1 of flasktaskr tutorial. bug free and functioning well.

--- app/views.py
+++ app/views.py
@@ -1,16 +1,17 @@
 # views.py
 
 
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 from functools import wraps
 import sqlite3
+from forms import AddTaskForm
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 
 def connect_db():
-    return sqlite.connect(app.config['DATABASE'])
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 def login_required(test):
@@ -41,3 +42,64 @@ def login():
             return redirect(url_for('tasks'))
     if request.method == 'GET':
         return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=1'
+    )
+    open_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    cur = g.db.execute(
+        'select name, due_date, priority, task_id from tasks where status=0'
+    )
+    closed_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template(
+        'tasks.html',
+        form=AddTaskForm(request.form),
+        open_tasks=open_tasks,
+        closed_tasks=closed_tasks
+    )
+
+@app.route('/add', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute('insert into tasks (name, due_date, priority, status) values (?, ?, ?, 1)', [name, date, priority])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('tasks'))
+
+# mark tasks as complete
+@app.route('/complete/<int:task_id>/',)
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.db.execute(
+        'update tasks set status = 0 where task_id=' + str(task_id)
+    )
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete.')
+    return redirect(url_for('tasks'))
+
+# delete tasks
+@app.route('/delete/<int:task_id>',)
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.db.execute('delete from tasks where task_id=' + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was deleted.')
+    return redirect(url_for('tasks'))
